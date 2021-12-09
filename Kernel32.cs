b22a3fc6cac54b2803aee54baf7b422cdc04eb14
Sr. Meia-noite { λ } (LordZarkares) <73501736+LordZarkares@users.noreply.github.com>
2021-12-09 05:19:55 -0300

    Create Kernel32.cs

---
+++ Kernel32.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Runtime.InteropServices;
+
+namespace MemoryManagement
+{
+    public static class Kernel32
+    {
+        public const int PROCESS_VM_OPERATION = (0x0008);
+        public const int PROCESS_VM_READ      = (0x0010);
+        public const int PROCESS_VM_WRITE     = (0x0020);
+
+        [DllImport("Kernel32.dll")]
+        public static extern IntPtr OpenProcess(int dwDesiredAccess, bool bInheritHandle, int dwProcessId);
+
+        [DllImport("Kernel32.dll")]
+        public static extern bool ReadProcessMemory(IntPtr hProcess, IntPtr lpBaseAddress, [Out, MarshalAs(UnmanagedType.AsAny)] object lpBuffer, int nSize, out int lpNumberOfBytesRead);
+
+    }
+}
