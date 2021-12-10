ccea2c9b48f295e606b2429c29bba116a1b15428
Sr. Meia-noite { λ } (LordZarkares) <73501736+LordZarkares@users.noreply.github.com>
2021-12-10 03:58:27 -0300

    to uint + things

--- User32.cs
+++ User32.cs
@@ -9,6 +9,12 @@ namespace MemoryManagement
         public const int WS_EX_LAYERED     = (0x080000);
         public const int GWL_EXSTYLE       = (-0000020);
 
+        public const int VK_LCONTROL = (0xA2);
+        public const int VK_RBUTTON  = (0x01);
+        public const int VK_LBUTTON  = (0x02);
+        public const int VK_DELETE   = (0x2E);
+        public const int VK_LSHIFT   = (0xA0);
+
         [StructLayout(LayoutKind.Sequential)]
         public struct RECT
         {
@@ -18,16 +24,20 @@ namespace MemoryManagement
             public int bottom ;
         }
 
-        [DllImport("user32.dll")]
-        public static extern int GetWindowLong(IntPtr hWnd, int nIndex);
+        [DllImport("User32.dll")]
+        public static extern int GetAsyncKeyState(int vKeys);
+        public static bool IsKeyDown(int vKey) => (GetAsyncKeyState(vKey) & 0x8000) != 0;
 
-        [DllImport("user32.dll")]
-        public static extern int SetWindowLong(IntPtr hWnd, int nIndex, int dwNewLong);
+        [DllImport("User32.dll")]
+        public static extern int GetWindowLong(UIntPtr hWnd, int nIndex);
 
-        [DllImport("user32.dll")]
+        [DllImport("User32.dll")]
+        public static extern int SetWindowLong(UIntPtr hWnd, int nIndex, int dwNewLong);
+
+        [DllImport("User32.dll")]
         public static extern IntPtr FindWindow(string lpClassName, string lpWindowName);
 
-        [DllImport("user32.dll")] [return : MarshalAs(UnmanagedType.Bool)]
-        public static extern bool GetWindowRect(IntPtr hWnd, out RECT lpRect);
+        [DllImport("User32.dll")] [return : MarshalAs(UnmanagedType.Bool)]
+        public static extern bool GetWindowRect(UIntPtr hWnd, out RECT lpRect);
     }
 }
