e8e400d750da5b27f2177cf6d78d661fdd8d7ec9
Sr. Meia-noite { λ } (LordZarkares) <73501736+LordZarkares@users.noreply.github.com>
2021-12-10 03:58:55 -0300

    to uint

--- Kernel32.cs
+++ Kernel32.cs
@@ -6,16 +6,25 @@ namespace MemoryManagement
     public static class Kernel32
     {
         public const int PROCESS_VM_OPERATION = (0x0008);
-        public const int PROCESS_VM_READ      = (0x0010);
         public const int PROCESS_VM_WRITE     = (0x0020);
+        public const int PROCESS_VM_READ      = (0x0010);
 
         [DllImport("Kernel32.dll")]
         public static extern IntPtr OpenProcess(int dwDesiredAccess, bool bInheritHandle, int dwProcessId);
 
+        
         [DllImport("Kernel32.dll")]
         public static extern bool ReadProcessMemory(IntPtr hProcess, IntPtr lpBaseAddress, byte[] lpBuffer, int nSize, out int lpNumberOfBytesRead);
 
         [DllImport("Kernel32.dll")]
+        public static extern bool ReadProcessMemory(IntPtr hProcess, UIntPtr lpBaseAddress, byte[] lpBuffer, int nSize, out int lpNumberOfBytesRead);
+
+
+        [DllImport("Kernel32.dll")]
         public static extern bool WriteProcessMemory(IntPtr hProcess, IntPtr lpBaseAddress, byte[] lpBuffer, int nSize, out int lpNumberOfBytetsWritten);
+        
+        [DllImport("Kernel32.dll")]
+        public static extern bool WriteProcessMemory(IntPtr hProcess, UIntPtr lpBaseAddress, byte[] lpBuffer, int nSize, out int lpNumberOfBytetsWritten);
+
     }
 }
