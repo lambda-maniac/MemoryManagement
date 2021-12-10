e2bd5cf13df3819a57d6466c35a5078ad3e6fc42
Sr. Meia-noite { λ } (LordZarkares) <73501736+LordZarkares@users.noreply.github.com>
2021-12-10 03:57:14 -0300

    to uint

--- Module.cs
+++ Module.cs
@@ -4,11 +4,11 @@ namespace MemoryManagement
 {
     public class Module
     {
-        public string ModuleName  ;
-        public IntPtr BaseAddress ;
-        public uint   Size        ;
+        public string  ModuleName  ;
+        public UIntPtr BaseAddress ;
+        public uint    Size        ;
 
-        public Module(string moduleName, IntPtr baseAddress, uint size)
+        public Module(string moduleName, UIntPtr baseAddress, uint size)
         {
             ModuleName  = moduleName  ;
             BaseAddress = baseAddress ;
